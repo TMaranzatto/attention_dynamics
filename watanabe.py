769584ab6ac6011c932eec02eeb43d6cc7ffe6a6
TMaranzatto <maranzatto1@yahoo.com>
2025-12-16 17:35:51 -0500

    Update watanabe.py
    -added OA phase space visualization

--- watanabe.py
+++ watanabe.py
@@ -3,8 +3,14 @@ import streamlit as st
 import numpy as np
 from math import pi
 from scipy.integrate import solve_ivp
+from matplotlib.animation import FuncAnimation, PillowWriter
 import matplotlib.pyplot as plt
-
+from random import choice
+from sympy import (
+    Symbol, symbols, Matrix, im, re, exp,
+    conjugate, simplify, lambdify
+)
+import time
 st.set_page_config(page_title="Attention Dynamics", layout="wide")
 
 st.title("N particles on the unit circle — integration of (Eq. 2.6) and the WS parameters")
@@ -14,7 +20,7 @@ with st.sidebar:
     st.header("Simulation controls")
     N = st.number_input("Number of particles N", min_value=1, max_value=500, value=20, step=1)
     T = st.number_input("End time (seconds)", min_value=0.1, value=10.0, step=1.)
-    frames = st.slider("Number of time samples", min_value=50, max_value=1001, value=401, step=50)
+    frames = st.slider("Number of time samples", min_value=500, max_value=5000, value=1000, step=50)
 
     st.markdown("---")
     st.subheader("Inverse temperature β")
@@ -45,7 +51,14 @@ with st.sidebar:
     A = np.array([[A11, A12],
                   [A21, A22]], dtype=float)
 
+    mult = np.matmul(V, A)
+    A_eigen = np.round(np.linalg.eig(A).eigenvectors, decimals=2)
+    V_eigen = np.round(np.linalg.eig(V).eigenvectors, decimals=2)
+    mult_eigen = np.round(np.linalg.eig(mult).eigenvectors, decimals=2)
+    st.write("V eigenvectors:", V_eigen, "A eigenvectors:", A_eigen, "A * B eigenvectors:", mult_eigen)
+
     st.markdown("---")
+
     if "seed" not in st.session_state:
         st.session_state.seed = np.random.SeedSequence().entropy
     if st.button("Randomize initial angles"):
@@ -55,7 +68,7 @@ with st.sidebar:
 
     w0 = V[0, 0] * A[1, 0] + V[0, 1] * A[1, 1] - V[1, 0] * A[0, 0] - V[1, 1] * A[0, 1]
     w1 = V[0, 0] * A[0, 0] + V[0, 1] * A[0, 1] - V[1, 0] * A[1, 0] - V[1, 1] * A[1, 1]
-    w2 = -V[0, 0] * A[1, 0] - V[0, 1] * A[1, 1] - V[1, 0] * A[0, 0] - V[1, 1] * A[0, 1]
+    w2 =-V[0, 0] * A[1, 0] - V[0, 1] * A[1, 1] - V[1, 0] * A[0, 0] - V[1, 1] * A[0, 1]
     w3 = V[0, 0] * A[1, 0] - V[0, 1] * A[1, 1] - V[1, 0] * A[0, 0] + V[1, 1] * A[0, 1]
     w4 = V[0, 0] * A[1, 1] + V[0, 1] * A[1, 0] - V[1, 0] * A[0, 1] - V[1, 1] * A[0, 0]
     w5 = V[0, 0] * A[0, 0] - V[0, 1] * A[0, 1] - V[1, 0] * A[1, 0] + V[1, 1] * A[1, 1]
@@ -68,8 +81,20 @@ with st.sidebar:
     b3 = 1 / 8 * (1j * w1 - w2)
 
     c1 = 1 / 8 * (-w3 + 1j * w4)
-    c2 = 1 / 4 * w0
+    c2 = - 1 / 4 * w0
 
+    # OA ODE
+    rho = symbols('ρ', real=True)
+    phi = symbols('ϕ', real=True)
+    R1 = im(b1 * exp(2j * phi) + b2)
+    R2 = im(b3 * exp(1j * phi))
+    I1 = re(b1 * exp(2j * phi) + b2)
+    I2 = re(b3 * exp(1j * phi))
+    I3 = re(c1 * exp(1j * phi))
+
+    rhodot = 2 * beta * (1 - rho ** 2) * (R1 * rho + R2)
+    phidot = 2 * beta * ((rho ** 2 + 1) * I1 + (rho ** 2 + 1) / rho * I2 + 2 * I3 * rho + re(c2))
+    print(beta)
 # Helper: wrap angles to [-pi, pi]
 def wrap_angles(x):
     return ((x + np.pi) % (2*np.pi)) - np.pi
@@ -132,6 +157,20 @@ def make_rhs():
 
     return rhs
 
+def angles_from_WS_variables(rho, phi, eta, psis):
+    #in the forward map, each psi corresponds to theta mod pi
+    #to start, I'll flip a coin for each psi to see if we add pi or not..
+    #there has to be a more principled way to do this.
+    zeta = rho * np.exp(1j * phi)
+    trial = [True, False]
+    def get_theta(psi):
+        ret = 0.5 * np.angle((zeta + np.exp(1j * (psi + eta))) / (1. + np.conj(zeta) * np.exp(1j * (psi + eta))))
+        if choice(trial):
+            ret += pi
+        return ret
+    vectorized = np.vectorize(get_theta)
+    return  vectorized(psis)
+
 def WS_variables_from_angles(thetas):
     R2 = np.mean(np.exp(2j * thetas))
     #we have many choices over these, following convention from Gong+Pikovsky 2020 https://arxiv.org/pdf/1909.07718
@@ -144,10 +183,26 @@ def WS_variables_from_angles(thetas):
     vectorized = np.vectorize(get_phi)
     return np.r_[rho_0, phi_0, eta_0, vectorized(thetas)]
 
-# Random initial angles
-rng = np.random.default_rng(int(st.session_state.seed) % (2**32))
-theta0 = rng.uniform(-np.pi/2, np.pi, size=int(N))
-WS_variables = WS_variables_from_angles(theta0)
+def random_thetas():
+    # Random initial angles
+    rng = np.random.default_rng(int(st.session_state.seed) % (2**32))
+    thetas = rng.uniform(0, 2*np.pi, size=int(N))
+    WSs = WS_variables_from_angles(thetas)
+    return thetas, WSs
+
+
+def random_WSs():
+    rng = np.random.default_rng(int(st.session_state.seed) % (2 ** 32))
+    psis = rng.uniform(0, 2*np.pi, size=int(N))
+    eta_0 = 0
+    phi_0 = 0
+    rho_0 = .01
+    thetas = angles_from_WS_variables(rho_0, phi_0, eta_0, psis)
+    print(np.mean(np.exp(2j*thetas)))
+    return thetas, np.r_[rho_0, phi_0, eta_0, psis]
+
+
+theta0, WS_variables = random_thetas()
 
 #
 # Integrate ORIGINAL DYNAMICS
@@ -183,7 +238,7 @@ except Exception as e:
 total_ws_path = wrap_angles(sol_ws.y)  # shape (N, len(t_eval))
 
 for i in [1, 2]:
-    THRESHOLD = 2 * pi - 0.05
+    THRESHOLD = 2 * pi - .01
     # Calculate difference between consecutive y and x values
     param_path = total_ws_path[i, :]
     dy = np.diff(param_path)
@@ -205,6 +260,7 @@ ax3[0].plot(sol_ws.t, total_ws_path[0])
 ax3[0].set_ylabel("WS amplitude ρ")
 ax3[0].set_ylim(0, 1)
 ax3[0].grid(alpha=0.3)
+ax3[0].set_xlim(0, float(T))
 
 #this follows basin analysis from Gong-Pikovsky
 ax3[1].plot(sol_ws.t, total_ws_path[1] / 2 + pi/2, color='teal', label = r'Φ/2 + $\pi/2$')
@@ -215,13 +271,15 @@ ax3[1].set_yticklabels([r"$-\pi/2$", "0", r"$\pi/2$"])
 ax3[1].set_ylim(-pi, pi)
 ax3[1].grid(alpha=0.3)
 ax3[1].legend()
+ax3[1].set_xlim(0, float(T))
 
 ax3[2].plot(sol_ws.t, total_ws_path[2])
-ax3[2].set_ylabel("WS parameter ρ")
+ax3[2].set_ylabel("WS parameter η")
 ax3[2].set_yticks([-np.pi / 2, 0, np.pi / 2])
 ax3[2].set_yticklabels([r"$-\pi/2$", "0", r"$\pi/2$"])
 ax3[2].set_ylim(-pi, pi)
 ax3[2].grid(alpha=0.3)
+ax3[2].set_xlim(0, float(T))
 
 ax3[2].set_xlabel("time")
 
@@ -251,12 +309,97 @@ ax.set_ylim(-np.pi, np.pi)
 ax.set_xlabel("time")
 ax.set_ylabel("angle (radians)")
 ax.set_title(f"{N} sample paths of angles (0 ≤ t ≤ {T})")
-ax.grid(alpha=0.3)
+#ax.grid(alpha=0.3)
 yticks = [-np.pi, -np.pi/2, 0, np.pi/2, np.pi]
 ax.set_yticks(yticks)
 ax.set_yticklabels([r"$-\pi$", r"$-\pi/2$", "0", r"$\pi/2$", r"$\pi$"])
 
 st.pyplot(fig)
+
+#
+#
+#now compute and plot OA vector Field animation
+#
+#
+rhodot_f = lambdify((rho, phi), rhodot, "numpy")
+phidot_f = lambdify((rho, phi), phidot, "numpy")
+
+rho_vals = np.linspace(.05, 1, 100)
+phi_vals = np.linspace(-np.pi, np.pi, 100)
+RHO, PHI = np.meshgrid(rho_vals, phi_vals)
+
+RHOdot = rhodot_f(RHO, PHI)
+PHIdot = phidot_f(RHO, PHI)
+if type(RHOdot) == int:
+    RHOdot = RHOdot * np.ones([100,100])
+if type(PHIdot) == int:
+    PHIdot = PHIdot * np.ones([100,100])
+speed = np.sqrt(RHOdot**2 + PHIdot**2)
+lw = 5*speed / speed.max()
+figOA, axOA = plt.subplots(figsize=(10, 6))
+axOA.streamplot(
+    RHO, PHI,
+    RHOdot, PHIdot,
+    density= .5,
+    linewidth=.5,
+    broken_streamlines=False,
+    color = 'k'
+)
+rho_min, rho_max = 0.05, 1
+axOA.set_xlabel(r"$\rho$")
+axOA.set_ylabel(r"$\phi$")
+axOA.set_xlim(rho_min, rho_max)
+axOA.set_ylim(-np.pi, np.pi)
+axOA.set_title("OA phase space vector field")
+axOA.grid(True)
+axOA.set_yticks(yticks)
+axOA.set_yticklabels([r"$-\pi$", r"$-\pi/2$", "0", r"$\pi/2$", r"$\pi$"])
+st.pyplot(figOA)
+
+if False:
+    #REMOVE THE CODE BELOW TO STOP THE ANIMATION...
+    N_OA = 400
+
+
+    rho_p = np.random.uniform(rho_min, rho_max, N_OA)
+    phi_p = np.random.uniform(-np.pi, np.pi, N_OA)
+
+    dt = 0.05
+
+    scat = axOA.scatter(rho_p, phi_p, s=10, alpha=1)
+
+    def update(frame):
+        global rho_p, phi_p
+
+        drho = rhodot_f(rho_p, phi_p)
+        dphi = phidot_f(rho_p, phi_p)
+
+        rho_p = rho_p + dt * drho
+        phi_p = wrap_angles(phi_p + dt * dphi)
+
+        # prevent rho collapse
+        rho_p = np.clip(rho_p, 0.05, 1)
+
+        scat.set_offsets(np.column_stack((rho_p, phi_p)))
+        return scat,
+    print('done computing')
+    ani = FuncAnimation(
+        figOA,
+        update,
+        frames=50,
+        interval=100,
+        blit=False
+    )
+    print('done animating')
+    ani.save('file_name.gif', writer=PillowWriter(fps=10))
+    print('Done Converting')
+    st.image('file_name.gif')
+    #END ANIMATION CODE
+
+
+
+
+
 
 #now plot WS
 with st.expander("Show integrated WS equations"):
@@ -276,4 +419,6 @@ with st.expander("Show order-parameters"):
     ax2[1].set_xlabel("time")
     ax2[1].set_ylim(0, 1)
     ax2[1].grid(alpha=0.3)
+    ax2[0].set_xlim(0, float(T))
+    ax2[1].set_xlim(0, float(T))
     st.pyplot(fig2)
